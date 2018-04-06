3f3344424595035cc26ddb25f2a80cafebee544a
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-06 16:18:23 +0100

    added guest name test

--- guest.rb
+++ guest.rb
@@ -1,7 +1,13 @@
 class Guest
 
-def initialize
+def initialize(name, money, favorite_song)
+  @name = name
+  @money = money
+  @favorite_song = favorite_song
+end
 
+def name()
+  return @name
 end
 
 end #end of class
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,5 +6,11 @@ require_relative("../guest.rb")
 class GuestTest < MiniTest::Test
 
   def setup()
-
+    @guest1 = Guest.new("Ricky", 50, "Ruby Tuesday")
   end
+
+  def test_guest_name
+    assert_equal("Ricky", @guest1.name)
+  end
+
+end #end of class
